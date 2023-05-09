8d4ab2148e900f69f5cf9e14b1d9c7f956eb1924
advakhov <megashunt@gmail.com>
2023-05-09 20:28:46 +0300

    09 May

---
+++ 2 lesson/task #1.py
@@ -0,0 +1,23 @@
+a = 14
+b = 8
+print(b)
+print (id(b))
+#команда чтобы узнать тип данных
+print(type(a))
+print(a + b)
+print(a - b)
+#деление целочисленное
+print(a // b)
+print(a * b)
+#возведение в степень
+print(a ** b)
+print(a ** 1/2)
+print("Булевое значение")
+#оператор сравнения "=="  результат True or False
+c = b == a + 3  # c = bool
+print(c)
+first = False
+second = True
+print(first == second)
+
+import_this
